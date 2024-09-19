5037d959f2b8af5a61f24529aa5bc3c7fa9c0cec
Karungg <miftahfadilah71@gmail.com>
2024-09-19 14:32:51 +0700

    Update Request resource

--- app/Filament/Clusters/Request/Resources/RequestResource/Pages/EditRequest.php
+++ app/Filament/Clusters/Request/Resources/RequestResource/Pages/EditRequest.php
@@ -10,6 +10,22 @@ class EditRequest extends EditRecord
 {
     protected static string $resource = RequestResource::class;
 
+    protected function mutateFormDataBeforeFill(array $data): array
+    {
+        if (isset($data['location']) && $data['location'] == 'Dalam Kota') {
+            $data['condition'] = true;
+        } else {
+            $data['condition'] = false;
+        }
+
+        return $data;
+    }
+
+    protected function getRedirectUrl(): ?string
+    {
+        return $this->getResource()::getUrl('view', ['record' => $this->getRecord()]);
+    }
+
     protected function getHeaderActions(): array
     {
         return [
